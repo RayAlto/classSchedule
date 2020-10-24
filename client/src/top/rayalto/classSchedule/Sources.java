02452e58b48546e9f2cd54cca94aae6780dad8a3
RayAlto <tianjiayu668@gmail.com>
2020-10-24 13:12:48 +0800

    Move images to src/

--- client/src/top/rayalto/classSchedule/Sources.java
+++ client/src/top/rayalto/classSchedule/Sources.java
@@ -3,5 +3,5 @@ package top.rayalto.classSchedule;
 import javax.swing.ImageIcon;
 
 public class Sources {
-    public static final ImageIcon LOGIN_BANNER_IMAGE = new ImageIcon("src/top/rayalto/classSchedule/images/loginBanner.png");
+    public static final ImageIcon LOGIN_BANNER_IMAGE = new ImageIcon("src/images/loginBanner.png");
 }
